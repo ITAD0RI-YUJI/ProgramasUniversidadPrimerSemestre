ae2348a8497eed0f2070efa1729db6d17935918a
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2023-11-14 23:35:42 -0500

    Agregando bloqueo para la máquina, está incompleto

--- proyectoFinalTriqui.cpp
+++ proyectoFinalTriqui.cpp
@@ -2,21 +2,20 @@
 #include <ctime>
 #include <cstdlib> // Agregamos esta librería para la función rand()
 #include <string>
-using namespace std;
+using namespace std; 
 
 string nombresComputador[] = {"Asta" , "Itadori" , "Luffy" , "Goku" , "Naruto"}; //Nombres para el computador a la hora de jugar
 int nombreSize = sizeof(nombresComputador)/sizeof(nombresComputador[0]);
 
-int tablero[3][3];
-
+char tablero[3][3];
 
 string nombresSelector(){ // Elegir el nombre del PC al azar
     string nombreComputador;
-
+    
     int indiciAleatorio = rand() % nombreSize;
-
+    
     nombreComputador = nombresComputador[indiciAleatorio];
-
+    
     return nombreComputador;
 }
 
@@ -24,34 +23,86 @@ int lanzarDado(){
     int numeroDado = 0;
 
     numeroDado = rand() % 5 + 1;
-
+    
     return numeroDado;
 }
 
-bool tableroRlleno(){ // Verifica si el tablero está vacío o totalmente lleno
-    int lleno = false;
+bool bloquear(){
+    bool lanzarBloqueo = false;
 
     for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            if(tablero[i][j] == 0){
-                lleno = true;
+        if(((tablero[i][0] == 'O') && (tablero[i][1] == 'O') && (tablero[i][2] == 'O')) || 
+           ((tablero[i][0] == 'O') && (tablero[i][2] == 'O') && (tablero[i][1] == 'O')) ||
+           ((tablero[i][1] == 'O') && (tablero[i][2] == 'O') && (tablero[i][0] == 'O'))){
+            for(int k = 0; k < 3; k++){
+                if(tablero[i][k] != 'O'){
+                    tablero[i][k] = 'X';
+                    return !lanzarBloqueo;
+                }
+            }
+        }
+
+        if(((tablero[0][i] == 'O') && (tablero[1][i] == 'O') && (tablero[2][i] == 'O')) || 
+           ((tablero[0][i] == 'O') && (tablero[2][i] == 'O') && (tablero[1][i] == 'O')) ||
+           ((tablero[1][i] == 'O') && (tablero[2][i] == 'O') && (tablero[0][i] == 'O'))){
+            for(int k = 0; k < 3; k++){
+                if(tablero[k][i] != 'O'){
+                    tablero[k][i] = 'X';
+                    return !lanzarBloqueo;
+                }
+            }
+        }
+        
+        if(((tablero[0][0] == 'O') && (tablero[1][1] == 'O') && (tablero[2][2] == 'O')) || 
+           ((tablero[0][0] == 'O') && (tablero[2][2] == 'O') && (tablero[1][1] == 'O')) ||
+           ((tablero[1][1] == 'O') && (tablero[2][2] == 'O') && (tablero[0][0] == 'O'))){
+            for(int k = 0; k < 3; k++){
+                if(tablero[k][k] != 'O'){
+                    tablero[k][k] = 'X';
+                    return !lanzarBloqueo;
+                }
+            }
+        }
+        
+        if(((tablero[0][2] == 'O') && (tablero[1][1] == 'O') && (tablero[2][0] == 'O')) || 
+           ((tablero[0][2] == 'O') && (tablero[2][0] == 'O') && (tablero[1][1] == 'O')) ||
+           ((tablero[1][1] == 'O') && (tablero[2][0] == 'O') && (tablero[0][2] == 'O'))){
+            for(int k = 0; k < 3; k++){
+                if(tablero[k][k] != 'O'){
+                    tablero[k][k] = 'X';
+                    return !lanzarBloqueo;
+                }
             }
         }
     }
 
-    return lleno;
+    return lanzarBloqueo; // Lanza "false" por defecto
 }
 
-void movimientoTablero(){
+void movimientoComputador(){
     int filaAleatoria = rand() % 3;
     int columnaAleatoria = rand() % 3;
+    bool vacioValidar = true;
 
-    while(tablero[filaAleatoria][columnaAleatoria] != 0){
-        filaAleatoria = rand() % 3;
-        columnaAleatoria = rand() % 3;
+    if((tablero[filaAleatoria][columnaAleatoria] == 'X') || (tablero[filaAleatoria][columnaAleatoria] == 'O')){
+        vacioValidar = false;
     }
 
-    tablero[filaAleatoria][columnaAleatoria] = 2;
+    while(!vacioValidar){
+        filaAleatoria = rand() % 3;
+        columnaAleatoria = rand() % 3;
+
+        if((tablero[filaAleatoria][columnaAleatoria] == 'X') || (tablero[filaAleatoria][columnaAleatoria] == 'O')){
+            vacioValidar = false;
+        } 
+        else {
+            bloquear();
+            vacioValidar = true;
+        }
+    }
+
+    tablero[filaAleatoria][columnaAleatoria] = 'X';
+
 }
 
 int main(){
@@ -62,7 +113,6 @@ int main(){
 
     int dadoPersona = 0;
     int dadoCompu = 0;
-
     int turno = 0;
 
     cout<< "\n♧ Ingresar nombre para jugador 1: ";
@@ -77,28 +127,24 @@ int main(){
     } 
     while (dadoCompu == dadoPersona); // Si los dados llegan a ser iguales, repetirá el lanzamiento hasta que sean diferentes
 
-
     cout<< "\nResultado dado "<< nombrePersona<< " : "<< dadoPersona<< endl;
     cout<< "Resultado dado "<< nombreComputador<< " : "<< dadoCompu<< endl;
 
+    char aumentador = '1';
+    for(int i = 0; i < 3; i++){     // Definiendo las posiciones dentro de la matriz (rellenar de 1-9)
+        for(int j = 0; j < 3; j++){
+           tablero[i][j] = aumentador;
+           aumentador += 1;
+        }
+    }
 
-    //Definiendo las posiciones dentro de la matriz (rellenar de 1-9)
-    // int aumentador = 1;
-
-    // for(int i = 0; i < 3; i++){
-    //     for(int j = 0; j < 3; j++){
-    //        tablero[i][j] = aumentador;
-    //        aumentador++;
-    //     }
-    // }
     cout<< "\nTablero a jugar: "<< endl;
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            cout<< tablero[i][j]<< " ";
+            cout<< " "<< tablero[i][j]<< " ";
         }
         cout<< endl;
     }
-
 
     //Definiendo quien tendrá el primer turno
     if(dadoPersona > dadoCompu){
@@ -109,50 +155,35 @@ int main(){
         cout<< "\n♧ El primer turno corresponde a "<< nombreComputador<< endl;
         turno = 0;
     }
-    cout<< endl;
 
+    for(int i = 0; i < 9; i++){
+        char posicionPersona = '0';
 
-    //Bucle que contiene todo lo que es el "juego" colocar X u O
-    while(tableroRlleno()){
-        int colocarPersonaFila = 0;
-        int colocarPersonaColumna = 0;
-        int numPersona = 0; // Ficha del usuario
-
-        if(turno == 0){ // Si el turno el 0 es elturno del computador
+        if(turno == 0){
             cout<< "\nMovimiento de "<< nombreComputador<< endl;
-            movimientoTablero();
+            movimientoComputador();
         }
         else if(turno == 1){ // Si el turno el 1 es elturno de la persona
-            do{
-                cout<<"\n♧ Posición a colocar en fila de "<< nombrePersona<< ": ";
-                cin>> colocarPersonaFila;
-            
-                cout<<"\n♧ Posición a colocar en columna de "<< nombrePersona<< ": ";
-                cin>> colocarPersonaColumna;
-            
-                cout<<"\n♧ Valor a poner en la posición dada "<< nombrePersona<< ": ";
-                cin>> numPersona;
+            cout<<"\n♧ Valor donde quieres poner en el tablero "<< nombrePersona<< ": ";
+            cin>> posicionPersona;
 
-                if(tablero[colocarPersonaFila][colocarPersonaColumna] != 0){
-                    cout<< "\nLa posición seleccionada ya está ocupada, elige otra "<< endl;
-                }
-                else{
-                    tablero[colocarPersonaFila][colocarPersonaColumna] = numPersona;
-                    break;
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                    if(tablero[i][j] == posicionPersona){
+                        tablero[i][j] = 'O'; 
+                    }
                 }
             }
-            while(tablero[colocarPersonaFila][colocarPersonaColumna] != 0);
         }
-
         cout<< endl;
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                cout<< tablero[i][j]<< " ";
+                cout<< " "<< tablero[i][j]<< " ";
             }
             cout<< endl;
         }
 
-        turno = (turno + 1) % 2; 
+        turno = (turno + 1) % 2;
     }
 
     return 0;
