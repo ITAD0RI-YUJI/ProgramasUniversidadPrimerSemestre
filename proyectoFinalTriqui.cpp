7202baf5791f73687b2281dcda018f3fa0738948
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2023-11-25 16:06:15 -0500

    Agregando modificación a la validación de espacio lleno o vacío hacia el usuario

--- proyectoFinalTriqui.cpp
+++ proyectoFinalTriqui.cpp
@@ -7,172 +7,63 @@ using namespace std;
 string nombresComputador[] = {"Asta" , "Itadori" , "Luffy" , "Goku" , "Naruto"}; //Nombres para el computador a la hora de jugar
 int nombreSize = sizeof(nombresComputador)/sizeof(nombresComputador[0]);
 
-char tablero[3][3] = {'1','2','3','4','5','6','7','8','9'};
+string tablero[3][3] = {"1","2","3","4","5","6","7","8","9"};
+
 
 string nombresSelector(){ // Elegir el nombre del PC al azar
     string nombreComputador;
-    
+
     int indiciAleatorio = rand() % nombreSize;
-    
+
     nombreComputador = nombresComputador[indiciAleatorio];
-    
+
     return nombreComputador;
 }
+
 
 int lanzarDado(){
     int numeroDado = 0;
 
     numeroDado = rand() % 5 + 1;
-    
+
     return numeroDado;
 }
 
-// bool bloquear(){
-    
+
+// void movimientoComputador(){
+//     int filaAleatoria;
+//     int columnaAleatoria;
+
+//     do{
+//         filaAleatoria = rand() % 3;
+//         columnaAleatoria = rand() % 3;
+//     }
+//     while((tablero[filaAleatoria][columnaAleatoria] == "X") || (tablero[filaAleatoria][columnaAleatoria] == "O"));
+
+//     tablero[filaAleatoria][columnaAleatoria] = "X";
 // }
+
+void bloquear(int filaAleatoria , int columnaAleatoria){
+
+    
+}
+
 
 void movimientoComputador(){
     int filaAleatoria;
     int columnaAleatoria;
 
-    do{
-        filaAleatoria = rand() % 3;
-        columnaAleatoria = rand() % 3;
-    }
-    while((tablero[filaAleatoria][columnaAleatoria] == 'X') || (tablero[filaAleatoria][columnaAleatoria] == 'O'));
+    filaAleatoria = rand() % 3;
+    columnaAleatoria = rand() % 3;
 
-    tablero[filaAleatoria][columnaAleatoria] = 'X';
+    if((tablero[filaAleatoria][columnaAleatoria] == "X") || (tablero[filaAleatoria][columnaAleatoria] == "O")){
+        bloquear(filaAleatoria , columnaAleatoria);
+    }
+    else{
+        tablero[filaAleatoria][columnaAleatoria] = "X";
+    }
 }
 
-void validarEspacio(char tableroFunc[3][3]){
-    char posicionPersona = 0;
-
-    int fila;
-    int columna;
-
-    bool espacioVacio = true;
-
-    do{
-        espacioVacio = true;
-        
-        cout<< "\n♧ Ingresa la posición para jugar: ";
-        cin>> posicionPersona;
-
-        switch (posicionPersona)
-        {
-            case '1':
-                fila = 0;
-                columna = 0;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '2':
-                fila = 0;
-                columna = 1;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '3':
-                fila = 0;
-                columna = 2;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '4':
-                fila = 1;
-                columna = 0;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '5':
-                fila = 1;
-                columna = 1;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '6':
-                fila = 1;
-                columna = 2;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '7':
-                fila = 2;
-                columna = 0;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '8':
-                fila = 2;
-                columna = 1;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-            
-            case '9':
-                fila = 2;
-                columna = 2;
-
-                if(tableroFunc[fila][columna] == 'X' || tableroFunc[fila][columna] == 'O'){
-                    cout<< "♧ Esta posición está ocupada, ingresa otra "<< endl;
-
-                    espacioVacio = false;
-                }
-
-            break;
-        }
-    }
-    while(!espacioVacio);
-
-    tableroFunc[fila][columna] = 'O';
-}
 
 int main(){
     srand(time(NULL));
@@ -183,6 +74,9 @@ int main(){
     int dadoPersona = 0;
     int dadoCompu = 0;
     int turno = 0;
+
+    int filaPosicionUsuario;
+    int columnaPosicionUsuario;
 
     cout<< "\n♧ Ingresar nombre para jugador 1: ";
     cin>> nombrePersona;
@@ -206,7 +100,7 @@ int main(){
         }
         cout<< endl;
     }
-    
+
     //Definiendo quien tendrá el primer turno
     if(dadoPersona > dadoCompu){
         cout<< "\n♧ El primer turno corresponde a "<< nombrePersona<< endl;
@@ -223,7 +117,28 @@ int main(){
             movimientoComputador();
         }
         else if(turno == 1){ // Si el turno es 1, es el turno de la persona
-            validarEspacio(tablero);
+            bool espacioVacio = true;
+
+            do{
+                espacioVacio = true;
+
+                cout<< "\n♧ Ingresa la fila de la posición para jugar: ";
+                cin>> filaPosicionUsuario;
+
+                cout<< "\n♧ Ingresa la columna de la posición para jugar: ";
+                cin>> columnaPosicionUsuario;
+
+                if((tablero[filaPosicionUsuario][columnaPosicionUsuario] == "X") || (tablero[filaPosicionUsuario][columnaPosicionUsuario] == "O")){
+                    cout<< "♧ Ocupado, igresa otra posición "<< endl;
+
+                    espacioVacio = false;
+                }
+                else{
+                    tablero[filaPosicionUsuario][columnaPosicionUsuario] = "O";
+                }
+
+            }
+            while(!espacioVacio); // Mientras el espacio no esté vacío, no se terminará de ejeutar
         }
 
         cout<< endl;
